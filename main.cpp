fa880237a921469a528185165855b7b97eb9b359
gholt15104 <47359435+gholt15104@users.noreply.github.com>
2019-02-05 11:52:57 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,14 +5,18 @@
 using namespace std;
 
 int main() {
-  //
+  //seed number generator
   srand(static_cast<unsigned int>(time(0)));
+  //initiating sleep function
   unsigned int sleep(unsigned int seconds);
   
   int health = 10, attack, block, turn = 0, numTurns;  
-
+  
+  //intro
   cout << "Welcome to your first adventure.\n";
   cout << "Here, we'll test your defense ability.\n\n";
+  
+  //difficulty selection
   cout << "Select the battle mode\n";
   cout << "1 - Standard (4 turns)\n";
   cout << "2 - Random (3 - 10 turns)\n";
@@ -41,17 +45,19 @@ int main() {
   }
 
   
-  //sleep(2);
+  //start of game
   cout << "Get ready! Here comes the first attack.\n";
   while (turn < numTurns && health > 0)
   {    
+    sleep(2); 
     ++turn;
+    //generates random numbers for attack and block
     attack = rand() % 5;
     block = rand() % 5;
     
     if(block >= attack)
     {
-      cout << "You blocked the attack. Great job!\n\n";
+      cout << "You blocked the attack. Great job!\n";
     }
     else
     {
@@ -71,14 +77,15 @@ int main() {
       else
       {
         cout << "You need to go see a doctor!\n";
-      }      
-      cout << "Attack: " << attack << "\n";
-      cout << "Block: " << block << "\n";
-      cout << "Health: " << health << "\n\n";
-    }    
+      }     
+    }
+    //turn stats
+    cout << "Attack: " << attack << "\n";
+    cout << "Block: " << block << "\n";
+    cout << "Health: " << health << "\n\n";
   }
 
-
+  //end of game
   if(health > 0)
   {
     cout << "\nYou survived! Good job!\n";
